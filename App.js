50798747e3a6426cbc5b90f3d158e1dd0efb8ee2
Trystan Hendricks <dolkhound@gmx.com>
2021-08-06 19:23:26 -0400

    API call is working perfectly

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'https://api.cryptonator.com';
 export default function App() {
+  // get data from API
+  const fetchData = () => {
+    // make the the end uri variables
+    axios
+      .get('/api/ticker/btc-usd')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
